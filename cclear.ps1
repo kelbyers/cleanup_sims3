4f5de4bffe9588cedbdfe022f0887821d7ce5c59
kel <ruferto@gmail.com>
2019-01-23 10:42:07 -0600

    get thumbnails to clean

--- cclear.ps1
+++ cclear.ps1
@@ -41,10 +41,32 @@ function FindCacheFiles {
     FindCleanableFiles @cacheFilter
 }
 
+function FindThumbnails {
+    param ( $root )
+    $subDirs = @{
+        Path = $root.FullName;
+        Directory = $true
+    }
+    $thumbsDirs = (Get-ChildItem @subDirs).where({$_.Name -eq "Thumbnails"})
+    $thumbnails = @()
+    if ($thumbsDirs) {
+        Write-Host "Thumbnails dir: $($thumbsDir.FullName)"
+        $filter = @{
+            root = $thumbsDirs[0].FullName;
+            ext = "package";
+            names = @("CASThumbnails.package", "ObjectThumbnails.package")
+        }
+        $thumbnails = FindCleanableFiles @filter
+    }
+    return $thumbnails
+}
+
 $sims3dir = FindEaSims3In .
 Write-Host "Found $($sims3dir.FullName)"
 
 $a = FindCacheFiles $sims3dir
 Write-Host "Found files: $a"
+$b = FindThumbnails $sims3dir
+Write-Host "Found thumbnails: $b"
 
 read-host 'Press ENTER to continue...'  # aka Pause
